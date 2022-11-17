51f6b279f09cae37819e7bac76d5f7630d398a11
MartinRastrilla <rastrillamartin@gmail.com>
2022-11-17 19:49:31 -0300

    Modelo: Cliente, Bicicleta

--- src/Modelo/Bicicleta.java
+++ src/Modelo/Bicicleta.java
@@ -2,5 +2,59 @@ package Modelo;
 
 
 public class Bicicleta {
+    private int numSerie;
+    private String tipo;
+    private String color;
+    private Cliente dniDuenio;
+    private boolean activo;
 
+    public Bicicleta(int numSerie, String tipo, String color, Cliente dniDuenio, boolean activo) {
+        this.numSerie = numSerie;
+        this.tipo = tipo;
+        this.color = color;
+        this.dniDuenio = dniDuenio;
+        this.activo = activo;
+    }
+
+    public int getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(int numSerie) {
+        this.numSerie = numSerie;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Cliente getDniDuenio() {
+        return dniDuenio;
+    }
+
+    public void setDniDuenio(Cliente dniDuenio) {
+        this.dniDuenio = dniDuenio;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+    
 }
--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -7,6 +7,54 @@ public class Cliente {
     private String domicilio;
     private String telefono;
     private boolean activo;
+
+    public Cliente(int dni, String nombreCompleto, String domicilio, String telefono, boolean activo) {
+        this.dni = dni;
+        this.nombreCompleto = nombreCompleto;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.activo = activo;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
     
     
 }
