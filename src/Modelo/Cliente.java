79dd3834dd072c52851df0ff6e1bc3aa32a85fa4
MartinRastrilla <rastrillamartin@gmail.com>
2022-11-17 19:40:08 -0300

    Update

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -1,12 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Modelo;
 
 
 public class Cliente {
-
+    private int dni;
+    private String nombreCompleto;
+    private String domicilio;
+    private String telefono;
+    private boolean activo;
+    
+    
 }
