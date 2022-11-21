bd97fa7a998e952b67160c55fcb1f1d87e3c5942
GeraGarro <113718948+GeraGarro@users.noreply.github.com>
2022-11-20 22:58:53 -0300

    Incorporacion UML, clases Repuesto y Servicio
    Creacion de el diagrama UML sujeto a modificaciones con lo cual conlleva a modificaciones en las clases Modelos.
    Modelos de las clases Repuesto y servicio completado hasta el momento relacionado con el bd y el uml

--- src/Modelo/Repuesto.java
+++ src/Modelo/Repuesto.java
@@ -2,5 +2,57 @@ package Modelo;
 
 
 public class Repuesto {
+private String num_serie;
+private String descripcion;
+private float precio;
+private boolean activo;
+
+    public Repuesto(String num_serie, String descripcion, float precio, boolean activo) {
+        this.num_serie = num_serie;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.activo = activo;
+    }
+
+    public Repuesto() {
+    }
+
+    public String getNum_serie() {
+        return num_serie;
+    }
+
+    public void setNum_serie(String num_serie) {
+        this.num_serie = num_serie;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Repuesto{" + "num_serie=" + num_serie + ", descripcion=" + descripcion + ", precio=" + precio + '}';
+    }
+
 
 }
